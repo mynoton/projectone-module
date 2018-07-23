d72a36cd07a85aff9081b9c7e0ab9a1f728ad6e5
mynoton <41176437+mynoton@users.noreply.github.com>
2018-07-23 14:39:54 +0700

    updated variables for S3 and DynamoDB

--- modules/aws_dynamodb/variables.tf
+++ modules/aws_dynamodb/variables.tf
@@ -5,3 +5,23 @@
 variable "dynamodb_tbl_name" {
   description = "Define DynamoDB table name"
 }
+
+variable "dynamodb_read_capacity" {
+  default = "Define DynamoDB read capacity"
+}
+
+variable "dynamodb_write_capacity" {
+  default = 1
+}
+
+variable "dynamodb_hash_key" {
+  default = 1
+}
+
+variable "dynamodb_attribute_name" {
+  default = "Define DynamoDB attribute name"
+}
+
+variable "dynamodb_attribute_type" {
+  default = "Define DynamoDB attribute type"
+}
--- modules/aws_s3/variables.tf
+++ modules/aws_s3/variables.tf
@@ -5,3 +5,11 @@
 variable "s3_bucket_name" {
   description = "Define S3 bucket name"
 }
+
+variable "s3_versioning" {
+  default = "Define S3 versioning"
+}
+
+variable "s3_lifecycle" {
+  default = "Define S3 lifecycle"
+}
