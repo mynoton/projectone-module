91656feda069a7bdc6b29167b194014673b18bdd
mynoton <41176437+mynoton@users.noreply.github.com>
2018-07-24 17:42:09 +0700

    update azure modules

--- modules/azure_resource_group/variables.tf
+++ modules/azure_resource_group/variables.tf
@@ -4,15 +4,12 @@
 
 variable "resource_group_name" {
   description = "Azure Resource Group Name"
-  default     = "ProjectOne"
 }
 
 variable "resource_group_location" {
   description = "Azure Resource Group Location"
-  default     = "Southeast Asia"
 }
 
 variable "resource_group_tag_name" {
   description = "Azure Resource Group Tag Name"
-  default     = "ProjectOne"
 }
