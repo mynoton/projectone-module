4e92d8cb989e4a9017c5d1d288fd9a1dc758fa65
mynoton <41176437+mynoton@users.noreply.github.com>
2018-07-25 18:24:46 +0700

    added azure_virtual_machine module

---
+++ modules/azure_virtual_machine/main.tf
@@ -0,0 +1,47 @@
+#================================================================
+# Azure Virtual Machine
+#================================================================
+
+resource "azurerm_virtual_machine" "virtual_machine" {
+    name                  = "${var.vm_name}"
+    location              = "${var.vm_location}"
+    resource_group_name   = "${var.vm_resource_group_name}"
+    network_interface_ids = ["${var.vm_network_interface_id}"]
+    vm_size               = "${var.vm_size}"
+
+    storage_os_disk {
+        name              = "${var.vm_disk_name}"
+        caching           = "${var.vm_disk_caching}"
+        create_option     = "${var.vm_disk_create_option}"
+        managed_disk_type = "${var.vm_disk_type}"
+    }
+
+    storage_image_reference {
+        publisher = "${var.vm_image_publisher}"
+        offer     = "${var.vm_image_offer}"
+        sku       = "${var.vm_image_sku}"
+        version   = "${var.vm_image_version}"
+    }
+
+    os_profile {
+        computer_name  = "${var.vm_profile_com_name}"
+        admin_username = "${var.vm_profile_admin_username}"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "${var.vm_config_sshkey_path}"
+            key_data = "${var.vm_config_sshkey_data}"
+        }
+    }
+
+    boot_diagnostics {
+        enabled     = "true"
+        storage_uri = "${var.vm_diagnostic_storage_uri}"
+    }
+
+    tags {
+        environment = "${var.vm_tag_env}"
+    }
+}
---
+++ modules/azure_virtual_machine/variables.tf
@@ -0,0 +1,79 @@
+#================================================================
+# Azure Virtual Machine Variable
+#================================================================
+
+variable "vm_name" {
+  description = "Azure virtual machine name"
+}
+
+variable "vm_location" {
+  description = "Azure virtual machine location"
+}
+
+variable "vm_resource_group_name" {
+  description = "Azure virtual machine resource group name"
+}
+
+variable "vm_network_interface_id" {
+  description = "Azure virtual machine interface id"
+}
+
+variable "vm_size" {
+  description = "Azure virtual machine size"
+}
+
+variable "vm_disk_name" {
+  description = "Azure virtual machine disk name"
+}
+
+variable "vm_disk_caching" {
+  description = "Azure virtual machine disk caching"
+}
+
+variable "vm_disk_create_option" {
+  description = "Azure virtual machine disk create option"
+}
+
+variable "vm_disk_type" {
+  description = "Azure virtual machine disk type"
+}
+
+variable "vm_image_publisher" {
+  description = "Azure virtual machine image publisher"
+}
+
+variable "vm_image_offer" {
+  description = "Azure virtual machine image offer"
+}
+
+variable "vm_image_sku" {
+  description = "Azure virtual machine image sku"
+}
+
+variable "vm_image_version" {
+  description = "Azure virtual machine image version"
+}
+
+variable "vm_profile_com_name" {
+  description = "Azure virtual machine profile computer name"
+}
+
+variable "vm_profile_admin_username" {
+  description = "Azure virtual machine profile admin username"
+}
+
+variable "vm_config_sshkey_path" {
+  description = "Azure virtual machine configuration sshkey path"
+}
+
+variable "vm_config_sshkey_data" {
+  description = "Azure virtual machine configuration sshkey data"
+}
+
+variable "vm_diagnostic_storage_uri" {
+  description = "Azure virtual machine diagnostic storage uri"
+}
+
+variable "vm_tag_env" {
+  description = "Azure virtual machine tag environment"
+}
