f780906be24e78c9223b71a144dd16bc4a9bbf22
mynoton <41176437+mynoton@users.noreply.github.com>
2018-07-25 17:51:38 +0700

    added azure_storage_account_boot_diagnostic module

---
+++ modules/azure_storage_account_boot_diagnostic/main.tf
@@ -0,0 +1,26 @@
+#================================================================
+# Azure Storage Account with Random Unique Name
+#================================================================
+
+resource "random_id" "random_id" {
+  keepers = {
+      # Generate a new ID only when a new resource group is defined
+      resource_group = "${var.random_id_resource_group_name}"
+  }
+  byte_length = 8
+}
+
+#================================================================
+# Azure Storage Account
+#================================================================
+
+resource "azurerm_storage_account" "storage_account" {
+    name                     = "diag${random_id.randomId.hex}"
+    resource_group_name      = "${var.storage_account_resource_group_name}"
+    location                 = "${var.storage_account_location}"
+    account_replication_type = "${var.storage_account_rep_type}"
+    account_tier             = "${var.storage_account_tier}"
+    tags {
+        environment          = "${var.storage_account_tag_env}"
+    }
+}
---
+++ modules/azure_storage_account_boot_diagnostic/variables.tf
@@ -0,0 +1,27 @@
+#================================================================
+# Azure Storage Account Variable
+#================================================================
+
+variable "storage_random_id_resource_group_name" {
+  description = "Azure random id function resource group name"
+}
+
+variable "storage_account_resource_group_name" {
+  description = "Azure storage account resource group name"
+}
+
+variable "storage_account_location" {
+  description = "Azure storage account location"
+}
+
+variable "storage_account_rep_type" {
+  description = "Azure storage account replication type"
+}
+
+variable "storage_account_tier" {
+  description = "Azure storage account tier"
+}
+
+variable "storage_account_tag_env" {
+  description = "Azure storage account tag environment"
+}
