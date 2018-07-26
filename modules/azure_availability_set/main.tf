649870aea1b58710ff3dbab8435805126eb638bb
mynoton <41176437+mynoton@users.noreply.github.com>
2018-07-26 20:24:08 +0700

    update

--- modules/azure_availability_set/main.tf
+++ modules/azure_availability_set/main.tf
@@ -3,10 +3,10 @@
 #================================================================
 
 resource "azurerm_availability_set" "availability_set" {
-  name                         = "${var.availability_set_name}"
-  location                     = "${var.availability_set_location}"
-  resource_group_name          = "${var.availability_set_resource_group_name}"
-  platform_fault_domain_count  = "${var.availability_set_fault_count}"
-  platform_update_domain_count = "${var.availability_set_update_count}"
-  managed                      = "${var.availability_set_management}"
+  name                         = "${var.avset_name}"
+  location                     = "${var.avset_location}"
+  resource_group_name          = "${var.avset_resource_group_name}"
+  platform_fault_domain_count  = "${var.avset_fault_count}"
+  platform_update_domain_count = "${var.avset_update_count}"
+  managed                      = "${var.avset_management}"
 }
--- modules/azure_availability_set/variables.tf
+++ modules/azure_availability_set/variables.tf
@@ -2,26 +2,26 @@
 # Azure Availability Set Variable
 #================================================================
 
-variable "availability_set_name" {
+variable "avset_name" {
   description = "Azure availability set name"
 }
 
-variable "availability_set_location" {
+variable "avset_location" {
   description = "Azure availability set location"
 }
 
-variable "availability_set_resource_group_name" {
+variable "avset_resource_group_name" {
   description = "Azure availability set resource group name"
 }
 
-variable "availability_set_fault_count" {
+variable "avset_fault_count" {
   description = "Azure availability set fault count"
 }
 
-variable "availability_set_update_count" {
+variable "avset_update_count" {
   description = "Azure availability set update count"
 }
 
-variable "availability_set_management" {
+variable "avset_management" {
   description = "Azure availability set management"
 }
